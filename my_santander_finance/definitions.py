7b9cd44d1b7bbabb8ba202219be02a914f7ae5c0
opaniagu@gmail.com <opaniagu@gmail.com>
2022-07-09 17:47:52 -0300

    Add support to home dir and more

--- my_santander_finance/definitions.py
+++ my_santander_finance/definitions.py
@@ -2,3 +2,11 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
 # ROOT_DIR = Path(__file__).parent.parent
+
+# HOME_DIR is an object of class:
+#   - PosixPath: if you're running on Linux or macOS
+#   - WindowsPath: if you're running on Windows
+# You can print it directly (including inside formatted f-strings)
+# but if you want to perform string operations on it, you must first convert it to a string:
+# https://csatlas.com/python-os-user-home-directory/
+HOME_DIR = str(Path.home())
--- my_santander_finance/init.py
+++ my_santander_finance/init.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join
+
 import sqlalchemy
 
 from my_santander_finance.func_dir import create_dir
@@ -5,15 +8,16 @@ from my_santander_finance.settings import settings
 
 
 def init_dir():
+    create_dir(settings.LOCAL_DIR)
     create_dir(settings.DOWNLOAD_DIR)
     create_dir(settings.DOWNLOAD_CUENTA_DIR)
-    create_dir(settings.DOWNLOAD_CUENTA_DIR + '.old')
+    create_dir(settings.DOWNLOAD_CUENTA_DIR + ".old")
     create_dir(settings.CVS_TEMP_DIR)
 
 
 def init_sqlite():
     # conectar a la base de datos
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + settings.DATABASE_SQLITE
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(settings.LOCAL_DIR, settings.DATABASE_SQLITE)
     database_connection = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
 
     sql = """
@@ -36,15 +40,15 @@ def init_sqlite():
         database_connection.execute(sql)
     except sqlalchemy.exc.SQLAlchemyError as ex:
         # Silently ignore errors if table and index already exist
-        if str(ex).find('already exists') != -1:
+        if str(ex).find("already exists") != -1:
             pass
         else:
-            print('####')
-            print('Error sqlite')
+            print("####")
+            print("Error sqlite")
             print(ex)
-            error = str(ex.__dict__['orig'])
+            error = str(ex.__dict__["orig"])
             print(error)
-            print('####')
+            print("####")
 
     sql = """
             CREATE UNIQUE INDEX `index_1` ON debit (`fecha`, `descripcion`,`cuenta_sueldo`);
@@ -54,17 +58,27 @@ def init_sqlite():
         database_connection.execute(sql)
     except sqlalchemy.exc.SQLAlchemyError as ex:
         # Silently ignore errors if table and index already exist
-        if str(ex).find('already exists') != -1:
+        if str(ex).find("already exists") != -1:
             pass
         else:
-            print('####')
-            print('Error sqlite')
+            print("####")
+            print("Error sqlite")
             print(ex)
-            error = str(ex.__dict__['orig'])
+            error = str(ex.__dict__["orig"])
             print(error)
-            print('####')
+            print("####")
+
+
+def create_env_example():
+    env_file_path = join(settings.LOCAL_DIR, ".env.example")
+    if os.path.exists(env_file_path) is False:
+        with open(env_file_path, "w") as f:
+            f.write("DNI=12345678\n")
+            f.write("CLAVE=clave\n")
+            f.write("USUARIO=usuario\n")
 
 
 if __name__ == "__main__":
     init_dir()
+    create_env_example()
     init_sqlite()
